ef59b39f62562c3a1018f1b714228e1d563cd0a5
Ulin Nuha <ulinpoenya@gmail.com>
2025-02-17 15:01:33 +0800

    Create __init__.py

---
+++ zero_shot_learning/__init__.py
@@ -0,0 +1,7 @@
+# zero_shot_learning/__init__.py
+
+# Importing specific classes or functions to make them accessible
+from .ZSLModel import ZSLModel
+
+# You can also define package-level variables or functions here
+__version__ = "1.0.0"
